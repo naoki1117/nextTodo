95d96baf47178c572cfef19b63e24fb78de9b3bf
naoki1117 <105569021+naoki1117@users.noreply.github.com>
2022-06-19 06:58:45 +0900

    up

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Layout } from '../componets/Layout'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <Layout title='Home'>
+      {"hellow"}
+    </Layout>
+     
+  )
+}
+
+export default Home
